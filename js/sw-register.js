7e1c2a03d8f3d86905fd3a0da45093129d7addb4
bensongardner <bensongardner@yahoo.com>
2019-09-11 22:54:25 -0500

    task: Add sw.js and js/sw-register.js

---
+++ js/sw-register.js
@@ -0,0 +1,11 @@
+// Register a service worker.
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function(){
+    navigator.serviceWorker.register('/sw.js').then(function() {         
+      console.log('Registration worked!');
+    }).catch(function(error) {
+      console.log(error);
+    });
+  });
+}
---
+++ sw.js
@@ -0,0 +1,26 @@
+const staticCacheName = 'restaurant-static-v4';
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      console.log(cache);
+      return cache.addAll([
+        '/',
+        'restaurant.html',
+        'css/styles.css',
+        'imgs/',
+        'data/restaurants.json',
+        'js/'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log(caches);
+  event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+});
